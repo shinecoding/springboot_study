3408457fe2015823063ff17bec1eccf2505713eb
shinecoding <choiyoungeun@gmail.com>
2021-07-25 23:01:24 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("greeting")
+    public String greeting(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping()
+    public String 
+}
