c668db9d54b9da267ffa92a86061c36d167b5924
shinecoding <choiyoungeun@gmail.com>
2021-07-31 16:29:34 +0900

    validation

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Board;
 import com.example.demo.repository.BoardRepository;
+import com.example.demo.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,9 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
 
     @GetMapping("/list")
     public String list(Model model) {
@@ -38,9 +42,9 @@ public class BoardController {
     }
 
     @PostMapping("/form")
-    public String boardForm(@Valid Board board, BindingResult bindingresult){
-
-        if(bindingresult.hasErrors()){
+    public String boardForm(@Valid Board board, BindingResult bindingResult){
+        boardValidator.validate(board, bindingResult);
+        if(bindingResult.hasErrors()){
             return "board/form";
         }
         boardRepository.save(board);
