d7417fb814103754fe0611c67e4d916b816b34c5
shinecoding <choiyoungeun@gmail.com>
2021-08-07 16:53:46 +0900

    잘못된 페이징

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.example.demo.model.Board;
 import com.example.demo.repository.BoardRepository;
 import com.example.demo.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,9 +27,14 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-       List<Board> boards = boardRepository.findAll();
-        model.addAttribute("boards",boards);
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable) {
+       Page<Board> boards = boardRepository.findAll(pageable);
+       int startPage = Math.max(1, boards.getPageable().getPageNumber()-4);
+       int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber()+4);
+       model.addAttribute("startPage", startPage);
+       model.addAttribute("endPage", endPage);
+
+       model.addAttribute("boards",boards);
         return "board/list";
     }
 
