8ffc0014d34afceb686361d630ed4adb70a127ea
shinecoding <choiyoungeun@gmail.com>
2021-08-08 17:47:49 +0900

    Spring Security

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,18 +1,28 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -26,5 +36,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username,password,enabled "
+                        + "from user "
+                        + "where email = ?")
+                .authoritiesByUsernameQuery("select username, name "
+                        + "from authorities "
+                        + "where email = ?");
+    }
+/*
+Authentication 로그인
+Authorization 권한
+
+테이블 조인할 때
+@OneToOne
+ex) user - user_detail 사용자와 사용자 상세정보
+@OneToMany
+ex) user - board 사용자와 게시글 : 하나의 사용자가 여거 개시물 작성
+@ManyToOne
+ex) board - user 여러개의 게시물은 하나의 사용자에 의해 작성이 됨
+@ManyToMany
+ex) user - role 사용자와 권한 : 하나의 사용자는 여러개의 권한을 가질 수 있고, 반대로 하나의 권한은 여러개의 사용자에 의해 설정될 수 있다
+ */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
